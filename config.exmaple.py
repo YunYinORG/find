ace84cb9208d8800afb10fc4740a3737f79fc4ea
New Future <new_future@msn.cn>
2015-11-28 11:02:32 +0800

    短链接

---
+++ config.exmaple.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+# 数据库信息
+DB_TYPE = 'mysql'
+try:
+    import sae.const
+    IS_SAE = True
+    DB_NAME = sae.const.MYSQL_DB      # 数据库名
+    DB_USER = sae.const.MYSQL_USER    # 用户名
+    DB_PWD = sae.const.MYSQL_PASS    # 密码
+    DB_HOST = sae.const.MYSQL_HOST    # 主库域名（可读写）
+    DB_PORT = int(sae.const.MYSQL_PORT)    # 端口，类型为<type 'str'>，请根据框架要求自行转换为int
+    # sae.const.MYSQL_HOST_S
+except Exception:
+    IS_SAE = False
+    DB_HOST = 'localhost'
+    DB_USER = 'root'
+    DB_NAME = 'yunyinfind'
+    DB_PORT = 3306
+    DB_PWD = ''
+
+# 云印接口
+YUNYIN_KEY = "yunyincard"
+YUNYIN_API = "http://api.yunyin.org/"
+
+#微博接口
+WEIBO_KEY=2681865267
+
+# 邮件
+MAIL_SMTP = 'smtp.exmail.qq.com'
+MAIL_PORT = 465
+MAIL_USER = 'test@mail.yunyin.org'
+MAIL_PWD = ''
+
+# 短信
+SMS_ACCOUNT = ''
+SMS_APPID = ''
+SMS_TOKEN = ''
+SMS_NOTIFY = ''
+SMS_LOGIN = ''
+SMS_BIND = ''
+
+# cookie加密
+COOKIE_KEY = 'qhsdfsffffffffff75V4d7F-sdfsf.wN'
+
+# 学校正则
+REGX_SHOOL = (
+    '^(\d{7}|\d{10})$',  # 通用正则
+    '^(([1][0-5]\d{5})|([1|2]1201[0-5]\d{4}))$',  # 南开
+    '^[1-4]01[0-5]\d{6}$',  # 天大
+    '^((0[1-5][01][0-9])|(1[139][05][1-4]))1[2-5]\d{4}$'  # 天商职
+)
+
+#快速查看地址
+VIEW_BASE="http://find.yunyin.org/record/v/"
+
--- lib/url.py
+++ lib/url.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 # coding=utf-8
-from config import VIEW_BASE as BASE_URL
+from json import loads
+import uuid
+import urllib2
+from config import VIEW_BASE as BASE_URL, WEIBO_KEY
 __doc__ = "短链接管理"
 
 
 def create(find_id, lost_id):
     """创建快速找回URL"""
     head = str(lost_id) + hex(int(find_id))[1:]
-    import uuid
     s = uuid.uuid1().__str__().replace('-', '')
     token = head + s[0:(32 - len(head))]
     return token, BASE_URL + token
@@ -15,4 +17,12 @@ def create(find_id, lost_id):
 
 def short(url):
     """创建短链接"""
-    return url[7:22]
+    apiurl = 'https://api.weibo.com/2/short_url/shorten.json?source=%s&url_long=%s' % (WEIBO_KEY, url)
+    req = urllib2.Request(apiurl)
+    response = urllib2.urlopen(req)
+    result = response.read()
+    try:
+        result = loads(result)  # 转成对象
+        return result['urls'][0]['url_short'][7:]
+    except Exception:
+        return "find.yunyin.org"
